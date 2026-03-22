987194dc1fe1b73756f4d4107d394569472485a8
LUHCaprisun <travoughna1@gmail.com>
2026-03-22 17:17:18 -0400

    sddsa

--- FrontEnd/src/app/Views/navigation/navigation.ts
+++ FrontEnd/src/app/Views/navigation/navigation.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { NzInputModule } from 'ng-zorro-antd/input';
+
+
 
 @Component({
   selector: 'app-navigation',
-  imports: [],
+  imports: [NzInputModule],
   templateUrl: './navigation.html',
   styleUrl: './navigation.css',
 })
