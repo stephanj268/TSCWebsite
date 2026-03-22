30de6fee3f914c025f4437238918b66caffa176e
LUHCaprisun <travoughna1@gmail.com>
2026-03-22 17:00:15 -0400

    PlayGorund

--- FrontEnd/src/app/app.config.ts
+++ FrontEnd/src/app/app.config.ts
@@ -3,11 +3,18 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+import { en_US, provideNzI18n } from 'ng-zorro-antd/i18n';
+import { registerLocaleData } from '@angular/common';
+import en from '@angular/common/locales/en';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { provideHttpClient } from '@angular/common/http';
+
+registerLocaleData(en);
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZonelessChangeDetection(),
-    provideRouter(routes), provideClientHydration(withEventReplay())
+    provideRouter(routes), provideClientHydration(withEventReplay()), provideNzI18n(en_US), provideAnimationsAsync(), provideHttpClient()
   ]
 };
--- FrontEnd/src/app/app.ts
+++ FrontEnd/src/app/app.ts
@@ -1,9 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavigationComponent } from './navigation/navigation';
+import { NzButtonModule } from 'ng-zorro-antd/button';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavigationComponent, NzButtonModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
