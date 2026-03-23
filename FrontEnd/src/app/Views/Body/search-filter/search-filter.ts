12923268ed221a83fe2b93f317f42fa47df90949
LUHCaprisun <travoughna1@gmail.com>
2026-03-23 13:49:49 -0400

    TOurs

--- FrontEnd/src/app/Views/Body/search-filter/search-filter.ts
+++ FrontEnd/src/app/Views/Body/search-filter/search-filter.ts
@@ -1,12 +1,30 @@
 import { Component } from '@angular/core';
-import { NzButtonModule } from "ng-zorro-antd/button";
+import { NzButtonModule, NzButtonSize  } from "ng-zorro-antd/button";
+import { FormsModule } from '@angular/forms';
+import { NzSelectModule } from 'ng-zorro-antd/select';
+import { NzGridModule } from 'ng-zorro-antd/grid';
+import { NzInputNumberModule } from 'ng-zorro-antd/input-number';
+import { NzSliderModule } from 'ng-zorro-antd/slider';
+
+import { NzInputModule } from 'ng-zorro-antd/input';
+
 
 @Component({
   selector: 'app-search-filter',
-  imports: [NzButtonModule],
+  imports: [
+    NzButtonModule, 
+    NzInputModule,
+    NzSelectModule, 
+    FormsModule,
+    NzGridModule, 
+    NzInputNumberModule, 
+    NzSliderModule
+  ],
   templateUrl: './search-filter.html',
   styleUrl: './search-filter.css',
 })
 export class SearchFilter {
+  value1 = 1;
+  size: NzButtonSize = 'small';
 
 }
