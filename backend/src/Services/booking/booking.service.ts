3445cdc652db413cb412866b3ded182b064ac36a
LUHCaprisun <travoughna1@gmail.com>
2026-04-06 22:12:49 -0400

    scsac

--- backend/src/Services/booking/booking.service.ts
+++ backend/src/Services/booking/booking.service.ts
@@ -2,6 +2,7 @@ import express, {type Request, type Response } from 'express';
 
 export function getAllBookings(req: Request, res: Response){
     try {
+        res.send('dsas')
 
     } catch (err) {
         throw res.json(err);
