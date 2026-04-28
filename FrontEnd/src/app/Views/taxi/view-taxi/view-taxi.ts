60f8d332045a601c3b6baf437be89c08d92fad45
LUHCaprisun <travoughna1@gmail.com>
2026-04-27 21:01:29 -0400

    sacdcasascdas

--- FrontEnd/src/app/Views/taxi/view-taxi/view-taxi.ts
+++ FrontEnd/src/app/Views/taxi/view-taxi/view-taxi.ts
@@ -14,7 +14,8 @@ import { TaxiService, ITaxi } from '../../../service/taxi.service';
 
 export class ViewTaxiComponent implements OnInit {
 
-  taxi?: ITaxi;
+  taxi: any;
+  taxiOptions: any = [];
 
   selectedDate: string = '';
   selectedTime: string = '';
@@ -32,13 +33,19 @@ export class ViewTaxiComponent implements OnInit {
     const rawId = params.get('id');
     const id = Number(rawId);
 
-    console.log('RAW ID:', rawId);
-    console.log('NUMBER ID:', id);
-    console.log('ALL IDS:', this.taxiService.allTours.map(t => t._id));
+    // console.log('RAW ID:', rawId);
+    // console.log('NUMBER ID:', id);
+    // console.log('ALL IDS:', this.taxiService.allTours.map(t => t._id));
 
     this.taxi = this.taxiService.getTaxiById(id);
 
+    for(let i = 0; i<this.taxi.taxiOptions.length; i++){
+      this.taxiOptions.push(this.taxi.taxiOptions[i])
+    }
+
     console.log('RESULT:', this.taxi);
+
+
   });
 }
 
