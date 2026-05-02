438421bf9d5337acfb44fca213083e5d0ecb6306
LUHCaprisun <travoughna1@gmail.com>
2026-05-02 19:49:37 -0400

    sd vc vxc

--- FrontEnd/src/app/Views/footer/footer.ts
+++ FrontEnd/src/app/Views/footer/footer.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
 
+import { NzIconModule } from 'ng-zorro-antd/icon';
+
 @Component({
   selector: 'app-footer',
-  imports: [],
+  imports: [NzIconModule],
   templateUrl: './footer.html',
   styleUrl: './footer.css',
 })
