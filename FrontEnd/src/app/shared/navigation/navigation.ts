c9ce762ddc6cb3fa435eb6960a691da971b75c6f
LUHCaprisun <travoughna1@gmail.com>
2026-05-23 18:14:07 -0400

    dds

--- FrontEnd/src/app/shared/navigation/navigation.ts
+++ FrontEnd/src/app/shared/navigation/navigation.ts
@@ -1,7 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { NzInputModule } from 'ng-zorro-antd/input';
 import { NzIconModule } from 'ng-zorro-antd/icon';
+
+import { NzButtonModule } from 'ng-zorro-antd/button';
+import { NzDrawerModule } from 'ng-zorro-antd/drawer';
+
 import { RouterLink, RouterLinkActive } from "@angular/router";
+
 
 
 @Component({
@@ -9,12 +15,38 @@ import { RouterLink, RouterLinkActive } from "@angular/router";
   imports: [
     NzInputModule,
     NzIconModule,
+    NzDrawerModule,
+    NzButtonModule,
+
+    CommonModule,
     RouterLink,
     RouterLinkActive
-],
+  ],
   templateUrl: './navigation.html',
   styleUrl: './navigation.css',
+  
+  host: {
+    ngSkipHydration: 'true'
+  },
 })
-export class NavigationComponent {
+export class NavigationComponent implements OnInit {
+
+  visible: boolean = false;
+
+  constructor() {
+
+  }
+
+  ngOnInit(): void {
+  }
+
+  open(): void {
+    this.visible = true;
+    console.log(this.visible)
+  }
+
+  close(): void {
+    this.visible = false;
+  }
 
 }
